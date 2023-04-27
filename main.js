224b6602395c2d763074fd9fdad893e90b92a26e
unknown <Administrateur@STA6401478.exchange.ad.afpanet>
2023-04-27 16:47:16 +0200

    ecf page principale avancé

--- main.js
+++ main.js
@@ -4,23 +4,24 @@ const circles = document.querySelectorAll('.circle-one, .circle-two, .circle-thr
 
 
 circles.forEach(circle => {
-    circle.addEventListener('click', () => {
-      circle.classList.toggle('active');
-     });
+  circle.addEventListener('click', () => {
+    circle.classList.toggle('active');
+  });
+});
+
+circles.forEach(circle => {
+  circle.addEventListener('mouseover', () => {
+    if (!circle.classList.contains('active')) {
+      circle.style.transition = 'all 0.2s ease';
+      circles.forEach(circle => circle.style.animationPlayState = 'paused');
+    }
   });
 
-  circles.forEach(circle => {
-    circle.addEventListener('mouseover', () => {
-      if (!circle.classList.contains('active')) {
-        circles.forEach(circle => circle.style.animationPlayState = 'paused');
-      }
-    });
-  
-    circle.addEventListener('mouseout', () => {
-      if (!circle.classList.contains('active')) {
-        circles.forEach(circle => circle.style.animationPlayState = 'running');
-      }
-    });
+  circle.addEventListener('mouseout', () => {
+    if (!circle.classList.contains('active')) {
+      circle.style.transition = 'all 0.2s ease';
+      circles.forEach(circle => circle.style.animationPlayState = 'running');
+    }
   });
-  
-  
+});
+
